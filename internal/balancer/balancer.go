0b2aebf333455ef180b77891e6244ba21549c3e1
0x0FACED <https.genius@gmail.com>
2025-05-01 12:29:53 +0300

    refactor: added Release() method for Balancer iface (for least conn balancer)

--- internal/balancer/balancer.go
+++ internal/balancer/balancer.go
@@ -2,7 +2,15 @@ package balancer
 
 import "context"
 
+type BalancerType string
+
+const (
+	RoundRobin BalancerType = "round_robin"
+	LeastConn  BalancerType = "least_conn"
+)
+
 type Balancer interface {
 	Next() (string, error)
+	Release(addr string)
 	StartHealthCheckJob(ctx context.Context)
 }
