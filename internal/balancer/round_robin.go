f26275a528a631f4de6bb48c6f19ed3c6c3bbc19
0x0FACED <https.genius@gmail.com>
2025-05-01 09:52:02 +0300

    chore: updated Next() method impl, added impl of StartHealthCheckJob() using goroutines, tickers and context

--- internal/balancer/round_robin.go
+++ internal/balancer/round_robin.go
@@ -1,22 +1,43 @@
 package balancer
 
 import (
+	"context"
 	"errors"
-	"fmt"
+	"net/http"
 	"sync"
+	"time"
+
+	"github.com/0x0FACED/zlog"
+)
+
+var (
+	ErrNoBackends      = errors.New("no backends available")
+	ErrBackendNotAlive = errors.New("backend is not alive")
 )
 
 type RoundRobinBalancer struct {
-	backends []string
+	backends []*Backend
 	current  int
 
-	mu sync.Mutex
+	log *zlog.ZerologLogger
+
+	cancel context.CancelFunc
+	mu     sync.Mutex
 }
 
-func NewRoundRobinBalancer(backends []string) *RoundRobinBalancer {
+func NewRoundRobinBalancer(backends []string, log *zlog.ZerologLogger) *RoundRobinBalancer {
+	backendsList := make([]*Backend, len(backends))
+	for i, addr := range backends {
+		backendsList[i] = &Backend{
+			Addr:  addr,
+			Alive: true,
+		}
+	}
+
 	return &RoundRobinBalancer{
-		backends: backends,
+		backends: backendsList,
 		current:  0,
+		log:      log,
 	}
 }
 
@@ -25,16 +46,71 @@ func (b *RoundRobinBalancer) Next() (string, error) {
 	defer b.mu.Unlock()
 
 	if len(b.backends) == 0 {
-		return "", errors.New("no available backends")
+		b.log.Error().Msg("No backends available")
+		return "", ErrNoBackends
 	}
 
+	var counter int
+	for range b.backends {
+		if counter > len(b.backends) {
+			return "", ErrNoBackends
+		}
+		if b.backends[b.current].IsAlive() {
+			break
+		}
+		b.current = (b.current + 1) % len(b.backends)
+		counter++
+	}
 	backend := b.backends[b.current]
+
+	b.log.Debug().Str("addr", backend.Addr).Msg("[Next] is selected")
+	// double check
+	if !backend.IsAlive() {
+		return "", ErrBackendNotAlive
+	}
 	b.current = (b.current + 1) % len(b.backends)
 
-	fmt.Println("Selected backend:", backend) // test log
-	return backend, nil
+	return backend.Addr, nil
 }
 
-func (b *RoundRobinBalancer) HealthCheck() error {
-	return nil
+const healthCheckInterval = 3 * time.Second
+const healthCheckTimeout = 2 * time.Second
+
+func (b *RoundRobinBalancer) StartHealthCheckJob(ctx context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	b.cancel = cancel
+
+	for _, backend := range b.backends {
+		go func(bk *Backend) {
+			client := &http.Client{Timeout: healthCheckTimeout}
+
+			ticker := time.NewTicker(healthCheckInterval)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-ctx.Done():
+					b.log.Info().Str("addr", bk.Addr).Msg("[HealthCheck] stopped")
+					return
+				case <-ticker.C:
+					url := bk.Addr + "/ping"
+					resp, err := client.Get(url)
+					if err != nil {
+						b.log.Error().Str("addr", bk.Addr).Err(err).Msg("[HealthCheck] is DOWN")
+						bk.SetAlive(false)
+						continue
+					}
+
+					resp.Body.Close()
+					if resp.StatusCode == http.StatusOK {
+						b.log.Debug().Str("addr", bk.Addr).Int("code", resp.StatusCode).Msg("[HealthCheck] is UP")
+						bk.SetAlive(true)
+					} else {
+						b.log.Warn().Str("addr", bk.Addr).Int("code", resp.StatusCode).Msg("[HealthCheck] not ready")
+						bk.SetAlive(false)
+					}
+				}
+			}
+		}(backend)
+	}
 }
