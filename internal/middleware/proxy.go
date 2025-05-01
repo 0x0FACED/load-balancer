3daeb59ae7106a38abdf9fed3313ab86fa2e4e3c
0x0FACED <https.genius@gmail.com>
2025-05-01 12:28:56 +0300

    refactor: fully refactored proxy (added response observer to release connection after request is done for least conn balancer)

--- internal/middleware/proxy.go
+++ internal/middleware/proxy.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/0x0FACED/load-balancer/internal/balancer"
@@ -13,59 +14,66 @@ import (
 )
 
 type ProxyMiddleware struct {
-	proxy    *httputil.ReverseProxy
 	balancer balancer.Balancer
 }
 
 func NewProxyMiddleware(balancer balancer.Balancer) *ProxyMiddleware {
-	pm := &ProxyMiddleware{
+	return &ProxyMiddleware{
 		balancer: balancer,
 	}
-
-	pm.proxy = &httputil.ReverseProxy{
-		Director:     pm.director,
-		ErrorHandler: pm.errorHandler,
-		Transport: &http.Transport{
-			MaxIdleConnsPerHost: 100,
-			IdleConnTimeout:     90 * time.Second,
-			DisableCompression:  false,
-		},
-	}
-
-	return pm
 }
 
 func (m *ProxyMiddleware) Proxy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		m.proxy.ServeHTTP(w, r)
+		backendAddr, err := m.balancer.Next()
+		if err != nil {
+			httpcommon.JSONError(w, http.StatusServiceUnavailable, err)
+			return
+		}
+
+		backendURL, err := url.Parse(backendAddr)
+		if err != nil {
+			httpcommon.JSONError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		wrapped := &responseObserver{
+			ResponseWriter: w,
+			onFinish: func() {
+				if lcb, ok := m.balancer.(interface {
+					Release(string)
+				}); ok {
+					lcb.Release(backendAddr)
+				}
+			},
+		}
+		defer wrapped.finishOnce()
+
+		proxy := &httputil.ReverseProxy{
+			Director: func(req *http.Request) {
+				req.URL.Scheme = backendURL.Scheme
+				req.URL.Host = backendURL.Host
+				req.URL.Path = singleJoiningSlash(backendURL.Path, req.URL.Path)
+				req.Header.Set("X-Forwarded-Host", req.Host)
+
+				if _, ok := req.Header["User-Agent"]; !ok {
+					req.Header.Set("User-Agent", "")
+				}
+
+				log.Println("Proxying to", backendURL.String())
+			},
+			ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
+				httpcommon.JSONError(w, http.StatusServiceUnavailable, err)
+			},
+			Transport: &http.Transport{
+				MaxIdleConnsPerHost: 100,
+				IdleConnTimeout:     90 * time.Second,
+				DisableCompression:  false,
+			},
+		}
+
+		proxy.ServeHTTP(wrapped, r)
 	})
-}
-
-func (m *ProxyMiddleware) director(req *http.Request) {
-	backend, err := m.balancer.Next()
-	if err != nil {
-		return
-	}
-
-	backendURL, err := url.Parse(backend)
-	if err != nil {
-		return
-	}
-
-	req.URL.Scheme = backendURL.Scheme
-	req.URL.Host = backendURL.Host
-	req.URL.Path = singleJoiningSlash(backendURL.Path, req.URL.Path)
-	req.Header.Set("X-Forwarded-Host", req.Host)
-
-	if _, ok := req.Header["User-Agent"]; !ok {
-		req.Header.Set("User-Agent", "")
-	}
-
-	log.Println("Proxying to ", backendURL.String())
-}
-
-func (m *ProxyMiddleware) errorHandler(w http.ResponseWriter, r *http.Request, err error) {
-	httpcommon.JSONError(w, http.StatusServiceUnavailable, err)
 }
 
 func singleJoiningSlash(a, b string) string {
@@ -78,4 +86,28 @@ func singleJoiningSlash(a, b string) string {
 		return a + "/" + b
 	}
 	return a + b
+}
+
+type responseObserver struct {
+	http.ResponseWriter
+	onFinish func()
+	once     sync.Once
+}
+
+func (o *responseObserver) Write(b []byte) (int, error) {
+	o.finishOnce()
+	return o.ResponseWriter.Write(b)
+}
+
+func (o *responseObserver) WriteHeader(statusCode int) {
+	o.finishOnce()
+	o.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (o *responseObserver) finishOnce() {
+	o.once.Do(func() {
+		if o.onFinish != nil {
+			o.onFinish()
+		}
+	})
 }
