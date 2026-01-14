0fe346a3a6f9f43766e021f31cc191787ebbdf16
0x0FACED <https.genius@gmail.com>
2026-01-14 16:57:15 +0300

    refactor: added TODO comment with potential problem description

--- internal/pkg/httpcommon/client_id.go
+++ internal/pkg/httpcommon/client_id.go
@@ -6,8 +6,17 @@ import (
 )
 
 func ClientIDFromRequest(r *http.Request) string {
+	// TODO: Think about X-Client-Id and RemoteAddr.
+	//
+	// There is might be collision with header.
+	// Maybe its important to correlate client id and remoteaddr.
+	// Problem:
+	// 	User1 uses X-Client-ID: "uuidtest1" and has remoteaddr 1.1.1.1
+	// 	User2 uses X-Client-ID: "uuidtest1" and has remoteaddr 2.2.2.2
+	// Different users, but rate limiter will think its 1 user.
 	if r.Header.Get("X-Client-ID") != "" {
 		return r.Header.Get("X-Client-ID")
 	}
+
 	return strings.Split(r.RemoteAddr, ":")[0]
 }
